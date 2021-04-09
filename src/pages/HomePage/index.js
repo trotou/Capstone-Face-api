e67ffdbbdd9a18ed6902ffea2f92aeb48b778974
Mateus Costa <mateusdsilvacosta@outlook.com>
2021-04-08 21:28:02 -0300

    Add Topbar

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -7,7 +7,8 @@ const HomePage = () => {
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
-            transition={{ duration: 1 }}>
+            transition={{ duration: 1 }}
+        >
             <div>Login</div>
         </motion.div>
     );
